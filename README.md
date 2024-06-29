6fd1d637e7561917260b0211a176ace1723c43a9
Anton Kholomiov <anton.kholomiov@gmail.com>
2024-06-29 14:12:52 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Purescript OSC
+
+FFI dinfings to [oscjs](https://github.com/colinbdclark/osc.js/) library.
+With it we can send and receive OSC messages (Open Sound Control).
---
+++ src/Network/Osc.purs
@@ -0,0 +1,61 @@
+module Network.Osc
+  ( Port
+  , Osc (..)
+  , OscValue (..)
+  ) where
+
+import Prelude
+import Effect (Effect)
+import JSON
+import JSON.Object as JO
+import JSON.Array as JA
+import Data.Tuple (Tuple(..))
+
+type Port =
+  { send :: Osc -> Effect Unit
+  }
+
+type Osc =
+  { address :: String
+  , args :: Array OscValue
+  }
+
+data OscValue
+  = OscFloat Number
+  | OscInt Int
+  | OscBoolean Boolean
+  | OscString String
+
+newWebsocketPort :: String -> Effect Port
+newWebsocketPort url = do
+  port <- _oscNewWebSocketPort url
+  _oscPortOpen port
+  pure
+    { send: \msg -> _oscPortSend port (oscToJson msg)
+    }
+
+oscToJson :: Osc -> JSON
+oscToJson osc =
+  fromJObject $ JO.fromEntries
+    [ Tuple "address" (fromString osc.address)
+    , Tuple "args" (fromArray $ map oscValueToJson osc.args)
+    ]
+
+oscValueToJson :: OscValue -> JSON
+oscValueToJson = case _ of
+  OscFloat val -> oscArgJson "f" (fromNumber val)
+  OscInt val -> oscArgJson "i" (fromInt val)
+  OscBoolean val -> oscArgJson "b" (fromBoolean val)
+  OscString val -> oscArgJson "s" (fromString val)
+
+oscArgJson :: String -> JSON -> JSON
+oscArgJson ty val = fromJObject $ JO.fromEntries
+  [ Tuple "type" (fromString ty)
+  , Tuple "value" val
+  ]
+
+foreign import data PortType :: Type
+
+foreign import _oscNewWebSocketPort :: String -> Effect PortType
+foreign import _oscPortOpen :: PortType -> Effect Unit
+foreign import _oscPortSend :: PortType -> JSON -> Effect Unit
