25ca6a383dbb20ebef3af64d386ab9abed25393b
Anton Kholomiov <anton.kholomiov@gmail.com>
2024-06-29 14:17:10 +0300

    Adds js files

---
+++ src/Network/Osc.js
@@ -0,0 +1,9 @@
+"use strict";
+
+export const _oscNewWebSocketPort = url => () =>
+  new osc.WebSocketPort({
+      url: url
+  });
+
+export const _oscPortOpen = port => () => port.open();
+export const _oscPortSend = port => => msg => () => port.send(msg);
